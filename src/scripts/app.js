bb587db4a67fedfdb877e05ee4d70764dc5c1743
mrKing <MrKing>
2018-10-26 13:13:33 +0300

    editsbigpopaps

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,10 @@
-// require('./common/jquery-3.2.1.min');
+require('./common/jquery-3.2.1.min');
 // require('./common/jquery.maskedinput');
 require('./common/sliderinit');
 require('./common/photoswipe');
+
+// require('./common/scrolloverflow.min');
+// require('./common/fullpage');
 
 require('./common/customscript');
 
